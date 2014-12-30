37d3383d213112c25ea8d53cb98e36fade07dc3e
Pavel Zdenek <pavel@salsitasoft.com>
2014-12-30 22:23:06 +0100

    Integrating HockeyApp upload

---
+++ scripts/hockey.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DIST_LIST_NAME=$1
+
+CURL=curl
+API_ENDPOINT=https://rink.hockeyapp.net/api/2/apps/upload
+
+BUILD_SUBFOLDER=Build/Products/Debug
+APP_PATH=${BUILD_SUBFOLDER}/FlushCapacitor.app
+ZIP=${BUILD_SUBFOLDER)/FlushCapacitor.zip
+
+zip -r9 $ZIP $APP_PATH
+
+# HA_APP_TOKEN was secured by `travis encrypt` stored in .travis.yml
+
+$CURL $API_ENDPOINT \
+-H "X-HockeyAppToken: ${HA_APP_TOKEN}" \
+-F ipa="@${IPA}" \
+-F dsym="@${DSYM}" \
+-F notify=0 \
+-F status=2 \
+-F tags="${DIST_LIST_NAME}"
