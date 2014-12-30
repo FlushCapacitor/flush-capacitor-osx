7e6ad2abf30d2f99999d56157e0ff6aac139a320
Pavel Zdenek <pavel@salsitasoft.com>
2014-12-31 00:44:55 +0100

    Fixing upload

--- scripts/hockey.sh
+++ scripts/hockey.sh
@@ -1,24 +1,21 @@
-#!/bin/sh
+#!/bin/sh -x
 
 DIST_LIST_NAME=$1
 
 CURL=curl
 API_ENDPOINT=https://rink.hockeyapp.net/api/2/apps/upload
 
-pwd
 BUILD_SUBFOLDER=${TRAVIS_BUILD_DIR}/Build
-echo $BUILD_SUBFOLDER
 APP_PATH=${BUILD_SUBFOLDER}/FlushCapacitor.app
-ZIP=${BUILD_SUBFOLDER}/FlushCapacitor.zip
+ZIP=${BUILD_SUBFOLDER}/FlushCapacitor.app.zip
 
 zip -r9 $ZIP $APP_PATH
 
 # HA_APP_TOKEN was secured by `travis encrypt` stored in .travis.yml
 
 $CURL $API_ENDPOINT \
--H "X-HockeyAppToken: ${HA_APP_TOKEN}" \
--F ipa="@${IPA}" \
--F dsym="@${DSYM}" \
+-H "X-HockeyAppToken: $HA_APP_TOKEN" \
+-F ipa="@${ZIP}" \
 -F notify=0 \
 -F status=2 \
 -F tags="${DIST_LIST_NAME}"
